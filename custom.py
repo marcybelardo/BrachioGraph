f83248cac8f938aec369032854511209d3b6a153
Marceline Belardo <marcelinebelardo@gmail.com>
2025-05-13 10:53:18 +0300

    added custom bg parameters

---
+++ custom.py
@@ -0,0 +1,8 @@
+from brachiograph import BrachioGraph
+
+bg = BrachioGraph(
+        servo_1_parked_pw=1550,
+        servo_2_parked_pw=1425,
+        hysteresis_correction_1=10,
+        hysteresis_correction_2=10
+)
